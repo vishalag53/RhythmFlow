d4eac6511557b54cd9a4a66a70a389874bd92428
vishalag53 <vishal.agrawal1207@gmail.com>
2025-05-31 14:59:55 +0530

    Initial commit

---
+++ app/src/main/java/com/vishalag53/mp3/music/rhythmflow/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.vishalag53.mp3.music.rhythmflow
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.vishalag53.mp3.music.rhythmflow.ui.theme.RhythmFlowTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RhythmFlowTheme {
+
+            }
+        }
+    }
+}
